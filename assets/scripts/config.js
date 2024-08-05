0da71040084a92dee15a91fa340b6ede3a65b4cf
Latexeii <rehevi.tw@gmail.com>
2024-08-06 00:21:16 +0300

    Päivityksiä

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -16,6 +16,7 @@ Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in 
 Config.Staff = [
     {name: "Salonen | Alphaonef", description: "Omistaja & DEV", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
     {name: "Kangasmetsä | Oke212", description: "Omistaja & Länsimaa Modding", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
+    {name: "Lateksi | Latexeii", description: "Admin & DEV", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/894588279591161926/997613293034287236/unnamed__1_-removebg-preview.png"},
 
 
 ];
